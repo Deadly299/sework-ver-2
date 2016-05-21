dc4e17e757795c0a7df89b0896924b7fb2f86b0d
Deadly299 <Deadly299@gmail.com>
2016-05-21 17:12:29 +0500

    Добавил изменение и удаление для факультетов

--- modul/update.php
+++ modul/update.php
@@ -14,7 +14,7 @@ include("../layaut/header.php");
 <!-- ///////////////////////////////////////////////////////////////////////////////////////// -->
 
     <?php
-    $connect= pg_connect("host=localhost port=5432 dbname=sework user=postgres password=postgres");
+  /*  $connect= pg_connect("host=localhost port=5432 dbname=sework user=postgres password=postgres");
        $result_f = pg_query($connect, "SELECT  *FROM faculties WHERE  change_history IS NULL ORDER BY id_from_edu_base ");
         $number_f=1;
         //$mass_f=array();
@@ -30,6 +30,7 @@ include("../layaut/header.php");
         //$mass_f=array();
         while ($row_psql_d=pg_fetch_row($result_d))
         {
+          
          $mass_d[$number_d] = $row_psql_d;
          $number_d++;
         }//Заполняю массив и кафедрами, без историй 
@@ -151,6 +152,134 @@ include("../layaut/header.php");
 
         print '<h3> Добавленно факультетов: '.$col_f.'</h3>';
         print '<h3> Добавленно кафедр: '.$col_d.'</h3>';
+
+       /*$result = pg_query($connect, "INSERT INTO faculties('id_from_edu_base,name_fac,change_history)
+        VALUES ('$row['id']', '$row['name']') WHERE id =''");*/
+
+       $connect= pg_connect("host=localhost port=5432 dbname=sework user=postgres password=postgres");
+        $host='localhost'; // имя хоста (уточняется у провайдера)
+        $database='kontract'; // имя базы данных, которую вы должны создать
+        $user='root'; // заданное вами имя пользователя, либо определенное провайдером
+        $pswd='max_ddlol'; // заданный вами пароль
+         
+        $dbh = mysql_connect($host, $user, $pswd) or die("Не могу соединиться с MySQL.");
+        mysql_query("SET NAMES utf8");
+        mysql_select_db($database) or die("Не могу подключиться к базе.");
+
+
+       $result_f = pg_query($connect, "SELECT  *FROM faculties WHERE  change_history IS NULL AND date_expiration IS NULL  ORDER BY id_from_edu_base ");
+
+        $number_f=1;
+        $number_f_m=1;
+        $date_today = date("d/m/Y");
+          while ($row_psql_f=pg_fetch_row($result_f))
+          {
+             $mass_f_id[$number_f] = $row_psql_f[1];//массив с ID
+             $mass_f[$number_f] = $row_psql_f;//массив весь
+             $number_f++;
+          } //Заполняю массив и факультетами, без историй 
+           $last_n = $mass_f_id[$number_f-1];
+
+        $query = "SELECT * FROM `faculties`"; $res = mysql_query($query);
+          while($row_mysql = mysql_fetch_array($res))
+          {
+            $mass_f_id_m[$number_f_m] = $row_mysql[0]; 
+            $mass_f_m[$number_f_m] = $row_mysql;//массив весь
+            $number_f_m++;
+          } //Заполняю массив из mysql
+          
+        $raz_index_1 = array_diff($mass_f_id, $mass_f_id_m);
+        $raz_index_2 = array_diff($mass_f_id_m, $mass_f_id);
+        $merge_mass_id = array_merge($raz_index_1, $raz_index_2);
+        //print_r($merge_mass_id);
+
+      
+        $col_add = 0;
+        foreach ($merge_mass_id as $key => $value) //Добавление
+        {
+          if($value > $last_n)//добавление
+          {
+            
+       
+            for ($i=1; $i <= count($mass_f_id_m); $i++) 
+            { 
+                if(isset($mass_f_m[$i]['id']))
+                {
+                  //print($mass_f_m[$i]['id']);
+                  
+                  if($mass_f_m[$i]['id'] == $value)
+                  {
+                    $id = $mass_f_m[$i]['id'];
+                    $name = $mass_f_m[$i]['name'];
+                    $result = pg_query($connect, "INSERT INTO faculties(id_from_edu_base,name_fac) VALUES ($id,'$name');");
+                    $col_add++;
+                  }
+                }                
+            }
+
+          }
+     
+        } //КОНЕЦ Добавление
+
+        $zapros='';//удаление/update 
+        $i=0;
+        foreach ($merge_mass_id as $key_s => $value_s)
+        {
+          if($value_s <= $last_n)//добавление
+          {
+            $merge_mass_id_sm[$i] =$value_s;
+            $i++;
+          }
+        }
+        if(isset($merge_mass_id_sm))
+        {
+          for ($i=0; $i < count($merge_mass_id_sm)-1; $i++) 
+          { 
+              if(count($merge_mass_id_sm)>=1)
+              {
+                $zapros= $zapros.'id_from_edu_base = '.$merge_mass_id_sm[$i].' OR ';
+              }                       
+          }
+            if(isset($merge_mass_id_sm[$i]))
+            {
+              $zapros= $zapros.'id_from_edu_base = '.$merge_mass_id_sm[$i];
+              $result_f = pg_query($connect, "SELECT  *FROM faculties WHERE  change_history IS NULL AND date_expiration IS NULL AND $zapros  ORDER BY id_from_edu_base ");
+            }
+            while ($row_psql_f=pg_fetch_row($result_f))
+          {
+             print $mass_f_id = $row_psql_f[1];//массив с ID
+
+             
+            $update_user = pg_query($connect,"UPDATE faculties SET 
+              date_expiration = '$date_today'
+              WHERE id_from_edu_base = $mass_f_id
+              ");
+          
+          }
+
+         } //Конец удаления 
+          
+
+
+
+
+//        print '<h4 align="left">Новых факультетов: '.$col_add.'</h4>';
+
+
+        //print_r($raz_index);
+        /*if($number_f_m >= $number_f)
+        {
+          $priority_n = 'mysql- '.$number_f_m;
+        } 
+          else $priority_n = 'pgsql- '.$number_f;
+          
+        print '<h3> Цикл строиться по: '.$priority_n.'</h3>';*/
+        /* ///////////Условия/////////////Условия/////////////Условия/////////////Условия/////////////Условия*/
+
+
+
+
+
 
        /*$result = pg_query($connect, "INSERT INTO faculties('id_from_edu_base,name_fac,change_history)
         VALUES ('$row['id']', '$row['name']') WHERE id =''");*/
